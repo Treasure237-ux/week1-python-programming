0ab84be55c39422434832adcbea541334a1cf7de
Okirhie Treasure <itreasure237@gmail.com>
2025-07-31 00:46:55 +0100

    my week1 assignment

---
+++ index1.py
@@ -0,0 +1,16 @@
+num1 = float(input("input first num:"))
+num2 = float(input("input second num:"))
+
+sum = num1 + num2
+diff = num1 - num2
+prod = num1 * num2
+quot = num1 / num2
+area_of_rec = num1 * num2
+area_of_tri = 0.5 * num1 * num2
+
+print(f'sum is:{sum}')
+print(f'differene is:{diff}')
+print(f'product is:{prod}')
+print(f'quotient is:{quot}')
+print(f'area of rectangle: {area_of_rec}cm^2')
+print(f'area of triangle: {area_of_tri}')
